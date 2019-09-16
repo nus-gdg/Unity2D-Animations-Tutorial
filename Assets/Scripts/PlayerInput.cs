f4220733a36b21fc63b8af5cd26db90e7e3c5945
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-09-17 00:02:56 +0800

    Add project assets

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerInput : MonoBehaviour
+{
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = Input.GetAxis("Horizontal");
+        bool jump = Input.GetAxis("Jump") > 0;
+        controller.Move(value, false, jump);
+    }
+}
